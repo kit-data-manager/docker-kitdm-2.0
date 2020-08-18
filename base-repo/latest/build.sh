8cc4f459d0026fa267ac33b39e9fa2848aa825bb
Thomas Jejkal <thomas.jejkal@kit.edu>
2020-08-18 22:47:41 +0200

    Update build.sh

--- base-repo/latest/build.sh
+++ base-repo/latest/build.sh
@@ -7,6 +7,6 @@ git clone --recursive https://github.com/kit-data-manager/base-repo.git
 cd base-repo
 
 echo "Building project from source."
-./gradlew -Pclean-release build
+./gradlew build
 
 echo "Build done."
