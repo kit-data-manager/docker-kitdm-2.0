76b23815287c588309c1ed41436ff9db4692ff6a
Thomas Jejkal <thomas.jejkal@kit.edu>
2020-08-20 14:56:05 +0200

    Update run.sh

--- admin-service/latest/run.sh
+++ admin-service/latest/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /base-repo
-java -cp ".:./config:./base-repo.jar" -Dloader.path=file://`pwd`/base-repo.jar,./lib/,. -jar base-repo.jar
+cd /admin-service
+java -cp ".:./config:./admin-service.jar" -Dloader.path=file://`pwd`/admin-service.jar,./lib/,. -jar admin-service.jar
