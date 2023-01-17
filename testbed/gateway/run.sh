f0d03dea6765ef31e11fa192e688ec8c7c68ec58
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-01-17 14:26:06 +0100

    Finished initial setup of repo and monitoring, Started adding gateway

---
+++ testbed/gateway/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash"
+################################################################################
+# Run microservice '$REPO_NAME'
+# /
+# |- application.properties    - Default configuration for microservice
+# |- '$REPO_NAME'*.jar"        - Microservice
+# |- run.sh                    - Start script
+# |- lib/                      - Directory for plugins (if supported)
+# |- config/
+#    |- application.properties - Overwrites default configuration (optional)
+################################################################################
+
+################################################################################
+# Define jar file
+################################################################################
+jarFile=gateway.jar
+
+################################################################################
+# Determine directory of script.
+################################################################################
+ACTUAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$ACTUAL_DIR"
+
+################################################################################
+# Start micro service
+################################################################################
+
+java -cp ".:$jarFile" -Dloader.path="file://$ACTUAL_DIR/$jarFile,./lib/,." -jar $jarFile
